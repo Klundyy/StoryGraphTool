f537ce33f018fe708703f049084a9c56b75c4ab9
wklunder <wklunder@ucsc.edu>
2024-04-17 22:05:48 -0700

    complete link based story game

--- rules.js
+++ rules.js
@@ -1,23 +1,43 @@
 class Start extends Scene {
     create() {
-        this.engine.setTitle("Title goes here"); // TODO: replace this text using this.engine.storyData to find the story title
-        this.engine.addChoice("Begin the story");
+        this.engine.setTitle(this.engine.storyData.Title); // TODO: replace this text using this.engine.storyData to find the story title
+        this.engine.addChoice("Begin the story"); 
     }
 
     handleChoice() {
-        this.engine.gotoScene(Location, "Home"); // TODO: replace this text by the initial location of the story
+        this.engine.gotoScene(Location,this.engine.storyData.InitialLocation);
     }
 }
 
 class Location extends Scene {
     create(key) {
-        let locationData = undefined; // TODO: use `key` to get the data object for the current story location
-        this.engine.show("Body text goes here"); // TODO: replace this text by the Body of the location data
-        
-        if(true) { // TODO: check if the location has any Choices
-            for(let choice of ["example data"]) { // TODO: loop over the location's Choices
-                this.engine.addChoice("action text"); // TODO: use the Text of the choice
-                // TODO: add a useful second argument to addChoice so that the current code of handleChoice below works
+        let inventory = this.engine.storyData.Inventory;
+        let locationData = this.engine.storyData.Locations[key];
+        this.engine.show(locationData.Body);
+        if(locationData.Choices != undefined) { 
+            for(let choice of locationData.Choices) {
+                if(choice.GameWorldItem != undefined){
+                    let invin;
+                    for(let i of inventory){
+                        if(i == choice.GameWorldItem){
+                            invin = true;
+                            break;
+                        }
+                    }
+                    if(!invin){
+                    this.engine.addChoice(choice.Text,choice);
+                    }
+                }else if(choice.Lock != undefined){
+                    if(inventory != undefined){
+                        for(let i of inventory){
+                            if(choice.Lock == i){
+                                this.engine.addChoice(choice.Text,choice);
+                            }
+                        }
+                    }
+                } else {
+                    this.engine.addChoice(choice.Text,choice);
+                }
             }
         } else {
             this.engine.addChoice("The end.")
@@ -25,10 +45,29 @@ class Location extends Scene {
     }
 
     handleChoice(choice) {
-        if(choice) {
+        if(choice) {  
+            let inventory = this.engine.storyData.Inventory;
             this.engine.show("&gt; "+choice.Text);
-            this.engine.gotoScene(Location, choice.Target);
-        } else {
+            if(choice.GameWorldItem != undefined){
+                if(choice.GameWorldItem == "Axe" && !inventory.includes("Coin")){
+                    this.engine.gotoScene(Location, choice.Target[1]);
+                }else if(choice.GameWorldItem == "Axe"){
+                    this.engine.gotoScene(Location, choice.Target[0]);
+                    inventory.push(choice.GameWorldItem);
+                } else{
+                    inventory.push(choice.GameWorldItem);
+                    this.engine.gotoScene(Location, choice.Target); 
+                }
+            } else if(choice.Split == "Path"){
+                if(inventory.includes("Axe")){
+                    this.engine.gotoScene(Location,choice.Target[1]);
+                } else{
+                    this.engine.gotoScene(Location,choice.Target[0]);
+                }
+            } else {
+                this.engine.gotoScene(Location, choice.Target); 
+            }
+        } else{
             this.engine.gotoScene(End);
         }
     }
@@ -37,7 +76,8 @@ class Location extends Scene {
 class End extends Scene {
     create() {
         this.engine.show("<hr>");
-        this.engine.show(this.engine.storyData.Credits);
+        this.engine.show("Congratulations you beat the game!");
+        this.engine.show("You ended with these items " + this.engine.storyData.Inventory);
     }
 }
 
